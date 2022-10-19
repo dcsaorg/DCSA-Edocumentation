1f4db4f6d1574d7b83b6b282bfd80f916eaf6b9d
abf <abf@asseco.dk>
2022-10-19 17:52:01 +0200

    DDT-1229: implemented get SI summaries endpoint

--- edocumentation-service/src/main/java/org/dcsa/edocumentation/service/mapping/DocumentStatusMapper.java
+++ edocumentation-service/src/main/java/org/dcsa/edocumentation/service/mapping/DocumentStatusMapper.java
@@ -1,9 +1,11 @@
 package org.dcsa.edocumentation.service.mapping;
 
 import org.dcsa.edocumentation.transferobjects.enums.BkgDocumentStatus;
+import org.dcsa.edocumentation.transferobjects.enums.EblDocumentStatus;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface DocumentStatusMapper {
   org.dcsa.edocumentation.domain.persistence.entity.enums.BkgDocumentStatus toDomainBkgDocumentStatus(BkgDocumentStatus documentStatus);
+  org.dcsa.edocumentation.domain.persistence.entity.enums.EblDocumentStatus toDomainEblDocumentStatus(EblDocumentStatus documentStatus);
 }
