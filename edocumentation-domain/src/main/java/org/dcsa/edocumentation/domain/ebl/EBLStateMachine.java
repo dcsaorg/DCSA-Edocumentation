b371b785dc1e94177b575988c6c3f29f866671b5
Niels Thykier <nt@asseco.dk>
2022-11-04 11:04:29 +0100

    Merge *StateMachineImpl into StateMachine
    Signed-off-by: Niels Thykier <nt@asseco.dk>

--- edocumentation-domain/src/main/java/org/dcsa/edocumentation/domain/ebl/EBLStateMachine.java
+++ edocumentation-domain/src/main/java/org/dcsa/edocumentation/domain/ebl/EBLStateMachine.java
@@ -1,14 +1,57 @@
 package org.dcsa.edocumentation.domain.ebl;
 
+import lombok.Getter;
+import org.dcsa.edocumentation.domain.dfa.AbstractStateMachine;
+import org.dcsa.edocumentation.domain.dfa.CannotLeaveTerminalStateException;
+import org.dcsa.edocumentation.domain.dfa.DFADefinition;
+import org.dcsa.edocumentation.domain.dfa.TargetStateIsNotSuccessorException;
 import org.dcsa.edocumentation.domain.persistence.entity.enums.EblDocumentStatus;
+import org.dcsa.skernel.errors.exceptions.ConcreteRequestErrorMessageException;
 
-public interface EBLStateMachine {
+import static org.dcsa.edocumentation.domain.persistence.entity.enums.EblDocumentStatus.*;
 
+public class EBLStateMachine extends AbstractStateMachine<EblDocumentStatus> {
+
+  private static final DFADefinition<EblDocumentStatus> DEFAULT_EBL_DFA_DEFINITION = DFADefinition.builder(RECE)
+    .nonTerminalState(RECE).successorNodes(PENU, DRFT)
+    .nonTerminalState(PENU).successorNodes(RECE)
+    .nonTerminalState(DRFT).successorNodes(PENA, APPR)
+    .nonTerminalState(PENA).successorNodes(DRFT, APPR)
+    .nonTerminalState(APPR).successorNodes(ISSU)
+    .nonTerminalState(ISSU).successorNodes(SURR)
+    .nonTerminalState(SURR).successorNodes(VOID)
+    .terminalStates(VOID)
+    .build();
+
+  private static final DFADefinition<EblDocumentStatus> AMENDMENT_EBL_DFA_DEFINITION = DFADefinition.builder(RECE)
+    .nonTerminalState(RECE).successorNodes(DRFT)
+    .nonTerminalState(DRFT).successorNodes(APPR)
+    .nonTerminalState(APPR).successorNodes(ISSU)
+    .nonTerminalState(ISSU).successorNodes(SURR)
+    .nonTerminalState(SURR).successorNodes(VOID)
+    .terminalStates(VOID)
+    .unreachableStates(PENU, PENA)
+    .build();
+
+  @Getter
+  private final boolean isAmendmentFlow;
+
+  EBLStateMachine(boolean isAmendmentFlow) {
+    super(fromFlow(isAmendmentFlow).fromInitialState());
+    this.isAmendmentFlow = isAmendmentFlow;
+  }
+
+  EBLStateMachine(boolean isAmendmentFlow, EblDocumentStatus resumeState) {
+    super(fromFlow(isAmendmentFlow).resumeFromState(resumeState));
+    this.isAmendmentFlow = isAmendmentFlow;
+  }
 
   /**
    * Transition the document into its {@link EblDocumentStatus#RECE} state.
    */
-  void receive();
+  public void receive() {
+    transitionTo(RECE);
+  }
 
   /**
    * Transition the document into its {@link EblDocumentStatus#PENU} state.
@@ -16,7 +59,9 @@ public interface EBLStateMachine {
    * <p>This state is not supported in all EBL flows. E.g., it is not reachable
    * in the Amendment flow.</p>
    */
-  void pendingUpdate();
+  public void pendingUpdate() {
+    transitionTo(PENU);
+  }
 
   /**
    * Check whether the flow supports the {@link EblDocumentStatus#PENU} state.
@@ -25,12 +70,16 @@ public interface EBLStateMachine {
    * the EBL flow does not support this state at all. I.e., calling {@link #pendingUpdate()}
    * will trigger an exception causing an internal server error status.</p>
    */
-  boolean isPendingUpdateSupported();
+  public boolean isPendingUpdateSupported() {
+    return supportsState(PENU);
+  }
 
   /**
    * Transition the document into its {@link EblDocumentStatus#DRFT} state.
    */
-  void draft();
+  public void draft() {
+    transitionTo(DRFT);
+  }
 
   /**
    * Transition the document into its {@link EblDocumentStatus#PENA} state.
@@ -38,7 +87,9 @@ public interface EBLStateMachine {
    * <p>This state is not supported in all EBL flows. E.g., it is not reachable
    * in the Amendment flow.</p>
    */
-  void pendingApproval();
+  public void pendingApproval() {
+    transitionTo(PENA);
+  }
 
   /**
    * Check whether the flow supports the {@link EblDocumentStatus#PENA} state.
@@ -47,47 +98,46 @@ public interface EBLStateMachine {
    * the EBL flow does not support this state at all. I.e., calling {@link #pendingApproval()}
    * will trigger an exception causing an internal server error status.</p>
    */
-  boolean isPendingApprovalSupported();
+  public boolean isPendingApprovalSupported() {
+    return supportsState(PENA);
+  }
 
   /**
    * Transition the document into its {@link EblDocumentStatus#APPR} state.
    */
-  void approve();
+  public void approve() {
+    transitionTo(APPR);
+  }
 
   /**
    * Transition the document into its {@link EblDocumentStatus#ISSU} state.
    */
-  void issue();
+  public void issue() {
+    transitionTo(ISSU);
+  }
 
   /**
    * Transition the document into its {@link EblDocumentStatus#SURR} state.
    */
-  void surrender();
+  public void surrender() {
+    transitionTo(SURR);
+  }
 
   /**
    * Transition the document into its {@link EblDocumentStatus#VOID} state.
    */
   // "void" is a keyword and cannot be used as a method name.
-  void voidDocument();
-
-  /**
-   * @return The current status of the EBL document (SI or TRD)
-   */
-  EblDocumentStatus getCurrentStatus();
-
-  /**
-   * @return true if this state machine is in the amendment flow. Otherwise, false.
-   */
-  boolean isAmendmentFlow();
-
+  public void voidDocument() {
+    transitionTo(VOID);
+  }
 
   /**
    * Initialize a EBLStateMachine from an initial state in the "default" flow.
    *
    * @return A state machine in the initial state.
    */
-  static EBLStateMachine fromInitialStateUsingDefaultFlow() {
-    return new EBLStateMachineImpl(false);
+  public static EBLStateMachine fromInitialStateUsingDefaultFlow() {
+    return new EBLStateMachine(false);
   }
 
   /**
@@ -96,11 +146,9 @@ public interface EBLStateMachine {
    * @param currentStatus The current document status
    * @return A EBLStateMachine initialized in the given state.
    */
-  static EBLStateMachine resumeFromStateUsingDefaultFlow(EblDocumentStatus currentStatus) {
-    return new EBLStateMachineImpl(false, currentStatus);
+  public static EBLStateMachine resumeFromStateUsingDefaultFlow(EblDocumentStatus currentStatus) {
+    return new EBLStateMachine(false, currentStatus);
   }
-
-
 
   /**
    * Initialize a EBLStateMachine from an initial state in the "amendment" flow.
@@ -109,8 +157,8 @@ public interface EBLStateMachine {
    *
    * @return A state machine in the initial state.
    */
-  static EBLStateMachine fromInitialStateUsingAmendmentFlow() {
-    return new EBLStateMachineImpl(true);
+  public static EBLStateMachine fromInitialStateUsingAmendmentFlow() {
+    return new EBLStateMachine(true);
   }
 
   /**
@@ -121,7 +169,33 @@ public interface EBLStateMachine {
    * @param currentStatus The current document status
    * @return A EBLStateMachine initialized in the given state.
    */
-  static EBLStateMachine resumeFromStateUsingAmendmentFlow(EblDocumentStatus currentStatus) {
-    return new EBLStateMachineImpl(true, currentStatus);
+  public static EBLStateMachine resumeFromStateUsingAmendmentFlow(EblDocumentStatus currentStatus) {
+    return new EBLStateMachine(true, currentStatus);
   }
+
+
+  @Override
+  protected RuntimeException errorForAttemptLeavingToLeaveTerminalState(EblDocumentStatus currentState, EblDocumentStatus successorState, CannotLeaveTerminalStateException e) {
+    // Special-case for terminal states, where we can generate quite nice sounding
+    // messages such as "... because the document is void (VOID)".
+    return ConcreteRequestErrorMessageException.conflict(
+      "Cannot perform the requested action on the document as the document is "
+        + currentState.getValue().toLowerCase() + " (" + currentState.name() + ")",
+      e
+    );
+  }
+
+  @Override
+  protected RuntimeException errorForTargetStatNotListedAsSuccessor(EblDocumentStatus currentState, EblDocumentStatus successorState, TargetStateIsNotSuccessorException e) {
+    return ConcreteRequestErrorMessageException.conflict(
+      "It is not possible to perform the requested action on the booking with documentStatus ("
+        + currentState.name() + ").",
+      e
+    );
+  }
+
+  private static DFADefinition<EblDocumentStatus> fromFlow(boolean isAmendmentFlow) {
+    return isAmendmentFlow ? AMENDMENT_EBL_DFA_DEFINITION : DEFAULT_EBL_DFA_DEFINITION;
+  }
+
 }
