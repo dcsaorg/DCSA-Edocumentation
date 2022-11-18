5a02cad75a9663cee9ccb4ad4d1093cfc5a157a8
pimg <pim.gaemers@dcsa.org>
2022-11-18 11:11:53 +0100

    DDT-1294 removed comment that is no longer applicable

--- edocumentation-service/src/main/java/org/dcsa/edocumentation/service/mapping/CargoItemMapper.java
+++ edocumentation-service/src/main/java/org/dcsa/edocumentation/service/mapping/CargoItemMapper.java
@@ -7,6 +7,5 @@ import org.mapstruct.Mapper;
 @Mapper(componentModel = "spring")
 public interface CargoItemMapper {
 
-  //Need to generate the ID, since it can happen that two cargoItems have the same values and therefor cannot be added to the set
   CargoItem toDAO(CargoItemTO cargoItemTO);
 }
