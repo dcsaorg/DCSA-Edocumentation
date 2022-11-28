ce7eef2e47eef86449812c365bfe112e18fe4ae6
pimg <pim.gaemers@dcsa.org>
2022-11-28 10:36:43 +0100

    Added setpoint entity

--- edocumentation-domain/src/main/java/org/dcsa/edocumentation/domain/persistence/entity/ActiveReeferSettings.java
+++ edocumentation-domain/src/main/java/org/dcsa/edocumentation/domain/persistence/entity/ActiveReeferSettings.java
@@ -9,15 +9,8 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.Set;
 import java.util.UUID;
 
 @Data
@@ -86,4 +79,10 @@ public class ActiveReeferSettings {
 
   @Column(name = "extra_material", length = 500)
   private String extraMaterial;
+
+  @ToString.Exclude
+  @EqualsAndHashCode.Exclude
+  @OneToMany(cascade = CascadeType.ALL)
+  @JoinColumn(name = "active_reefer_settings_id", nullable = false, referencedColumnName = "id")
+  Set<Setpoint> setpoints;
 }
---
+++ edocumentation-domain/src/main/java/org/dcsa/edocumentation/domain/persistence/entity/Setpoint.java
@@ -0,0 +1,49 @@
+package org.dcsa.edocumentation.domain.persistence.entity;
+
+import lombok.*;
+import org.dcsa.edocumentation.domain.persistence.entity.enums.TemperatureUnit;
+import org.dcsa.edocumentation.domain.persistence.entity.enums.VolumeUnit;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Data
+@Builder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter(AccessLevel.PRIVATE)
+@Entity
+@Table(name = "setpoint")
+public class Setpoint {
+
+  @Id
+  @GeneratedValue
+  @Column(name = "id", nullable = false)
+  private UUID id;
+
+  @Column(name = "temperature")
+  private Float temperature;
+
+  @Column(name = "temperature_unit")
+  @Enumerated(EnumType.STRING)
+  private TemperatureUnit temperatureUnit;
+
+  @Column(name = "humidity")
+  private Float humidity;
+
+  @Column(name = "air_exchange")
+  private Float airExchange;
+
+  @Column(name = "air_exchange_unit")
+  @Enumerated(EnumType.STRING)
+  private VolumeUnit airExchangeUnit;
+
+  @Column(name = "o2")
+  private Float o2;
+
+  @Column(name = "co2")
+  private Float co2;
+
+  @Column(name = "days_prior_to_discharge")
+  private Float daysPriorToDischarge;
+}
