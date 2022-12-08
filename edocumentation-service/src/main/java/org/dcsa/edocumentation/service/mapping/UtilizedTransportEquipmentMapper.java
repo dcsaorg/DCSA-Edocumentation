2e571db9b94917464c117b673823ff978b99d263
Niels Thykier <nit@globeteam.com>
2022-12-08 16:40:15 +0100

    DDT-1368 [2/?]: Implement mapping of utilized transport equipment
    Signed-off-by: Niels Thykier <nit@globeteam.com>

--- edocumentation-service/src/main/java/org/dcsa/edocumentation/service/mapping/UtilizedTransportEquipmentMapper.java
+++ edocumentation-service/src/main/java/org/dcsa/edocumentation/service/mapping/UtilizedTransportEquipmentMapper.java
@@ -6,11 +6,18 @@ import org.dcsa.edocumentation.transferobjects.UtilizedTransportEquipmentTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", uses = {
+  ActiveReeferSettingsMapper.class,
+  EquipmentMapper.class,
+  SealMapper.class,
+})
 public interface UtilizedTransportEquipmentMapper {
 
   @Mapping(source = "utilizedTransportEquipmentTO.isShipperOwned", target = "isShipperOwned")
   @Mapping(source = "requestedEquipmentGroup", target = "requestedEquipmentGroup")
   @Mapping(source = "requestedEquipmentGroup.id", target = "id", ignore = true)
   UtilizedTransportEquipment toDAO(UtilizedTransportEquipmentTO utilizedTransportEquipmentTO, RequestedEquipmentGroup requestedEquipmentGroup);
+
+  @Mapping(source = "utilizedTransportEquipment.requestedEquipmentGroup.activeReeferSettings", target = "activeReeferSettings", qualifiedByName = "toShippingInstructionActiveReeferSettingsTO")
+  UtilizedTransportEquipmentTO toDTO(UtilizedTransportEquipment utilizedTransportEquipment);
 }
