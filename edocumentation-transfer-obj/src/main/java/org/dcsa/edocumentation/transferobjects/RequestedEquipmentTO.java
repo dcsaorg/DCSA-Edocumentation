0cff2d0ae9eaed9015dbe524aba95b880e74db8c
Finn Nielsen <finn.nielsen@dcsa.org>
2023-02-06 13:53:56 +0100

    DDT-1531

--- edocumentation-transfer-obj/src/main/java/org/dcsa/edocumentation/transferobjects/RequestedEquipmentTO.java
+++ edocumentation-transfer-obj/src/main/java/org/dcsa/edocumentation/transferobjects/RequestedEquipmentTO.java
@@ -7,10 +7,15 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import org.dcsa.edocumentation.transferobjects.enums.WeightUnit;
+import org.dcsa.skernel.infrastructure.validation.DisallowIfBoolean;
 import org.dcsa.skernel.infrastructure.validation.ISO6346EquipmentReference;
+import org.dcsa.skernel.infrastructure.validation.RequiredIfTrue;
 
 import java.util.List;
 
+@DisallowIfBoolean(ifField = "isShipperOwned", hasValue = false, thenDisallow = {"tareWeight", "tareWeightUnit"})
+@RequiredIfTrue(isFieldReferenceRequired = "isShipperOwned", fieldReference = "tareWeight")
+@RequiredIfTrue(isFieldReferenceRequired = "isShipperOwned", fieldReference = "tareWeightUnit")
 public record RequestedEquipmentTO(
   @NotBlank @Size(max = 4)
   @JsonProperty("ISOEquipmentCode")
