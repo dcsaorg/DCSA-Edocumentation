d9c6399e5083d5945413ad218ea35fcd34295e00
PreetamPotadar <preetam.potadar@dsca.org>
2023-09-25 16:52:17 +0200

    Merge branch 'master' into DT-211-Update-RI-BKG-EBL-Change-Customs-Filing
    # Conflicts:
    #	edocumentation-domain/src/main/resources/db/migration/V3_0__dcsa_im_v3.sql
    #	postman_collection.json

--- edocumentation-domain/src/main/resources/db/migration/V3_0__dcsa_im_v3.sql
+++ edocumentation-domain/src/main/resources/db/migration/V3_0__dcsa_im_v3.sql
@@ -572,3 +572,10 @@ CREATE TABLE shipment_transport (
     universal_service_reference varchar(8) NULL,
     is_under_shippers_responsibility boolean NOT NULL
 );
+  CREATE TABLE advance_manifest_filing (
+	manifest_id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
+    shipment_id uuid NOT NULL REFERENCES shipment(id),
+    manifest_type_code varchar(50) NOT NULL,
+    country_code varchar(2) NOT NULL,
+	list_order int NOT NULL default 0
+);
